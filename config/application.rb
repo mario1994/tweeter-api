5d3352dae0e507df9fa06457a8dae71afe44fec3
mlg-Mboban <mboban@mlg.tv>
2017-08-21 14:33:17 +0200

    added rack-cors

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,14 @@ Bundler.require(*Rails.groups)
 module TweeterApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
     config.load_defaults 5.1
 
     
@@ -31,9 +39,5 @@ module TweeterApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    config.middleware.insert_before 0, Rack::Cors do
-     allow do
-       origins '*'
-       resource '*', :headers => :any, :methods => [:get, :post, :options]
   end
 end
